8cd2979f0ac421c85b663cacf455b412739264b4
daniel j. white <dan@chalkie.org.uk>
2016-11-04 11:55:51 +0100

    fix time series measure script
    uses getStatistics instead of measure, and gets active ROI

--- Time_Series_Measure.py
+++ Time_Series_Measure.py
@@ -5,6 +5,7 @@
 
 # Dan White and Silke Gerwig MPI-CBG
 # Aug 2009
+# Dan White 2016 
 
 #!/usr/bin/env python
 
@@ -12,12 +13,14 @@ from ij import IJ, ImagePlus
 # the current image
 imp = IJ.getImage()
 stack = imp.getStack()
+roi = imp.getRoi()
 
-# for all the frames in the movie, run  histogram
-for i in range(1, imp.getNFrames() + 1):  # remember a python range (1, 10) is the numbers 1 to 9Ê!
+# for all the frames in the movie, run  measure
+for i in range(1, imp.getNFrames() + 1):  # remember a python range (1, 10) is the numbers 1 to 9!
 #getNFrames not getNSlices since input data is a 2D time series stack NOT a 3D stack.
-  slice = ImagePlus(str(i), stack.getProcessor(i))
-  # Execute plugin exactly on this slice i
-  IJ.run(slice, "Measure", ROI)
-  
+	frame = ImagePlus(str(i), stack.getProcessor(i))
+ 	# Execute get statistics exactly on this slice i, with the ROI in use
+	frame.setRoi(roi)
+	stats = frame.getStatistics()
+	IJ.log(" area: "+ str(stats.area) + " mean: "+ str(stats.mean))
 IJ.log("Done!")
