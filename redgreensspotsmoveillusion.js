b2fff6b8fc39ccd5504c3a8f7a51a59f687a27d7
daniel j. white <dan@chalkie.org.uk>
2016-11-04 14:29:59 +0100

    fix script so it works with current fiji, js imports.

--- redgreensspotsmoveillusion.js
+++ redgreensspotsmoveillusion.js
@@ -1,22 +1,33 @@
-imp = IJ.newImage("colourspotsillusion", "RGB black", 256, 256, 1);
+// Movement illusion
+// Look just above the flashing circles and they appear to move towards each other
+// but look directly at one of them and it only flashes in same position
+// can't remeber where I found this illusion.
 
-for (i=0; i<100; i++) {
-	imp.setRoi(new OvalRoi(171, 106, 43, 41));
-	IJ.run("Color Picker...", "");
+importClass(Packages.ij.IJ);
+importClass(Packages.ij.gui.OvalRoi);
+
+imp = IJ.createImage("ColourMoveIllusion", "RGB black", 256, 256, 1);
+imp.show();
+
+for (var i=0; i<100; i++) {
+	imp.setRoi(new OvalRoi(150, 106, 45, 45));
 	IJ.setForegroundColor(255, 0, 0);
 	IJ.run(imp, "Fill", "slice");
-	IJ.updateDisplay();
+	IJ.run(imp, "Select None", "");
 	IJ.wait(100);
 	IJ.setForegroundColor(0, 0, 0);
+	IJ.run("Restore Selection", "");
 	IJ.run(imp, "Fill", "slice");
-	IJ.updateDisplay();
-	imp.setRoi(new OvalRoi(46, 106, 45, 42));
+	IJ.run(imp, "Select None", "");
 	IJ.setForegroundColor(0, 255, 0);
+	imp.setRoi(new OvalRoi(80, 106, 45, 45));
 	IJ.run(imp, "Fill", "slice");
-	IJ.updateDisplay();
+	IJ.run(imp, "Select None", "");
 	IJ.wait(100);
 	IJ.setForegroundColor(0, 0, 0);
+	IJ.run("Restore Selection", "");
 	IJ.run(imp, "Fill", "slice");
-	IJ.updateDisplay();
+	IJ.run(imp, "Select None", "");
 	imp.show();
 	}
+imp.close();
